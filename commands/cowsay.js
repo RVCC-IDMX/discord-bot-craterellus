a5d0aa990980c4b1c5249e988fbb5473114b54e9
craterellus <98069492+craterellus@users.noreply.github.com>
2022-12-17 22:13:01 -0500

    Add cow options & error handling

--- commands/cowsay.js
+++ commands/cowsay.js
@@ -6,15 +6,40 @@ module.exports = {
     .setName('cowsay')
     .setDescription('Makes a cow say something!')
     .addStringOption((option) => option.setName('input')
-      .setDescription('The input to echo back')
-      .setMaxLength(25))
+      .setDescription('What the cow says')
+      .setMaxLength(50))
     .addStringOption((option) => option.setName('cow')
       .setDescription('Choose your cow')
-      .setMaxLength(25)),
+      .setMaxLength(25)
+      .addChoices(
+        { name: 'classic cowsay', value: 'default' },
+        { name: 'king', value: 'king' },
+        { name: 'spidercow', value: 'spidercow' },
+        { name: 'jellyfish', value: 'jellyfish' },
+        { name: 'sheep', value: 'sheep' },
+        { name: 'cowfee', value: 'cowfee' },
+        { name: 'ghost', value: 'ghost' },
+        { name: 'skeleton', value: 'skeleton' },
+        { name: 'wizard', value: 'wizard' },
+        { name: 'whale', value: 'whale' },
+        { name: 'sudowoodo', value: 'sudowoodo' },
+        { name: 'shrug', value: 'shrug' },
+        { name: 'goat', value: 'goat' },
+        { name: 'banana', value: 'banana' },
+        { name: 'dalek', value: 'dalek' },
+        { name: 'homer', value: 'homer' },
+        { name: 'ackbar', value: 'ackbar' },
+      )),
   async execute(interaction) {
     const userInput = interaction.options.getString('input');
     const cowChoice = interaction.options.getString('cow');
     const moo = cowsay.say({ text: userInput, f: cowChoice }).replaceAll('`', '\'');
-    await interaction.reply(`\`\`\`${moo}\`\`\``);
+    const reply = `\`\`\`${moo}\`\`\``;
+    const mooCount = reply.length;
+    if (mooCount < 2000) {
+      await interaction.reply(reply);
+    } else {
+      await interaction.reply('Error: Your cowsay is over the character limit!');
+    }
   },
 };
